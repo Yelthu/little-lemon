d4fe9a8560bc33df88e4e6ae63a2c4b51e55f7e1
Ye Lin Thu <yelinthu@Yes-MacBook-Pro-YNWA.local>
2024-08-02 15:45:14 +0700

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BookingPage from './components/BookingPage';
+import { initializeTimes } from './components/Main';
+import { reducer } from './components/Main';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders the BookingForm heading', () => {
+  render(<BookingPage />)
+  const headingElement = screen.getByText('Book Now');
+  expect(headingElement).toBeInTheDocument();
+})
+
+test('initializeTimes returns the correct expected value', () => {
+  const expectedValue = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  const result = initializeTimes();
+  expect(result).toEqual(expectedValue);
+})
+
+test('updateTimes changes available times based on selected date', () => {
+  const initialState = {
+    selectedDate: '2022-01-01',
+    availableTime: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+  }
+  const selectedDate = '2022-01-02';
+
+  const action = { type: 'UPDATE_TIMES', payload: selectedDate }
+  const result = reducer(initialState, action);
+  expect(result).toEqual({
+    ...initialState,
+    selectedDate: selectedDate
+  })
+})
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,34 @@ import Chicago from './Chicago';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import BookingPage from './BookingPage';
+import { useReducer } from 'react';
+
+export const initializeTimes = () => {
+    return  ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+}
+
+export const reducer = (state, action) => {
+    console.log('arrive from booking page ....', action)
+    switch (action.type) {
+        case 'UPDATE_TIMES':
+            return {
+               ...state,
+               selectedDate: action.payload
+            }
+        default:
+            return state;
+    }
+
+}
 
 const Main = () => {
+
+    const updateTimes = (selectedDate) => {
+        dispatch({ type: 'UPDATE_TIMES', payload: selectedDate });
+    };
+
+    const [availableTimes, dispatch] = useReducer(reducer, initializeTimes());
+
     return (
         <>
             <Router>
@@ -18,7 +44,7 @@ const Main = () => {
                         <Route path="/specials" element={<Specials />} />
                         <Route path="/testimonials" element={<CustomersSay />} />
                         <Route path="/about" element={<Chicago />} />
-                        <Route path="/booking" element={<BookingPage />}></Route>
+                        <Route path="/booking" element={<BookingPage availableTimes={availableTimes} updateTimes={updateTimes} />}></Route>
                     </Routes>
                 </div>
             </Router>
